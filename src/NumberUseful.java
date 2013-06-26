8b0fee112cfa9755f848dc78d5878c32b52883de
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2013-06-26 04:10:37 +0000

    put back outer loop (needed to handle e.g. 9/16)

--- src/NumberUseful.java
+++ src/NumberUseful.java
@@ -116,61 +116,63 @@ public class NumberUseful
                 Math.pow(Multiplier, Math.ceil(Places / Math.log10(Multiplier)))
               );
             int Numer = (int)Math.round(Val * Denom);
-            int BestNumer = Numer;
-            int BestDenom = Denom;
-            int BestGCD = gcd(Numer, Denom);
-            final int ILow = Numer - (int)Math.floor((Val * (1 - Tol) * Denom - Numer) * RangeFactor);
-            final int IHigh = Numer + (int)Math.ceil((Val * (1 + Tol) * Denom - Numer) * RangeFactor);
-            boolean IAscending = false;
-            for (int i = Numer;;)
+            for (;;)
               {
-                if (IAscending && i > IHigh)
-                    break;
-                if (!IAscending && i < ILow)
+                int BestNumer = Numer;
+                int BestDenom = Denom;
+                int BestGCD = gcd(Numer, Denom);
+                final int ILow = Numer - (int)Math.floor((Val * (1 - Tol) * Denom - Numer) * RangeFactor);
+                final int IHigh = Numer + (int)Math.ceil((Val * (1 + Tol) * Denom - Numer) * RangeFactor);
+                boolean IAscending = false;
+                for (int i = Numer;;)
                   {
-                    i = Numer + 1;
-                    IAscending = true;
-                  } /*if*/
-                final int JLow = Numer - (int)Math.floor(Numer / (Val * (1 + Tol)) * RangeFactor);
-                final int JHigh = Numer + (int)Math.ceil(Numer / (Val * (1 - Tol)) * RangeFactor);
-                boolean JAscending = false;
-                for (int j = Denom;;)
-                  {
-                    if (JAscending && j > JHigh)
+                    if (IAscending && i > IHigh)
                         break;
-                    if (!JAscending && j < JLow)
+                    if (!IAscending && i < ILow)
                       {
-                        j = Denom + 1;
-                        JAscending = true;
+                        i = Numer + 1;
+                        IAscending = true;
                       } /*if*/
-                    if
-                      (
-                            i > 0
-                        &&
-                            j > 0
-                        &&
-                            (i != Numer || j != Denom)
-                        &&
-                            Math.abs((i * 1.0 / j - Val) / Val) <= Tol
-                      )
+                    final int JLow = Numer - (int)Math.floor(Numer / (Val * (1 + Tol)) * RangeFactor);
+                    final int JHigh = Numer + (int)Math.ceil(Numer / (Val * (1 - Tol)) * RangeFactor);
+                    boolean JAscending = false;
+                    for (int j = Denom;;)
                       {
-                        final int ThisGCD = gcd(i, j);
-                        if (ThisGCD > BestGCD)
+                        if (JAscending && j > JHigh)
+                            break;
+                        if (!JAscending && j < JLow)
                           {
-                            BestNumer = i;
-                            BestDenom = j;
-                            BestGCD = ThisGCD;
+                            j = Denom + 1;
+                            JAscending = true;
                           } /*if*/
-                      } /*if*/
-                    j = JAscending ? j + 1 : j - 1;
+                        if
+                          (
+                                i > 0
+                            &&
+                                j > 0
+                            &&
+                                (i != Numer || j != Denom)
+                            &&
+                                Math.abs((i * 1.0 / j - Val) / Val) <= Tol
+                          )
+                          {
+                            final int ThisGCD = gcd(i, j);
+                            if (ThisGCD > BestGCD)
+                              {
+                                BestNumer = i;
+                                BestDenom = j;
+                                BestGCD = ThisGCD;
+                              } /*if*/
+                          } /*if*/
+                        j = JAscending ? j + 1 : j - 1;
+                      } /*for*/
+                    i = IAscending ? i + 1 : i - 1;
                   } /*for*/
-                i = IAscending ? i + 1 : i - 1;
-              } /*for*/
-            if (BestGCD > 1)
-              {
+                if (BestGCD == 1)
+                    break;
                 Denom = BestDenom / BestGCD;
                 Numer = BestNumer / BestGCD;
-              } /*if*/
+              } /*for*/
             return
                 new Fraction(Numer, Denom);
           } /*FromReal*/
